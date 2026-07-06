3b93c52826f58fd405dc78ce3410ca12656f5780
Kim Pepper <kim@pepper.id.au>
2026-07-07 09:19:42 +1000

    Bump dev deps to latest, PHP 8.2 min, drop console_table, untrack composer.lock
    - Set PHP minimum to 8.2
    - Bump dev dependencies to highest minor+patch (coder 9, phpcs 4, phpstan 2, phpunit 11)
    - Migrate phpunit.xml.dist to PHPUnit 11 schema
    - Convert @group annotation to #[Group] attribute
    - Remove abandoned pear/console_table and unused printTree() debug helper
    - Remove redundant always-true assert($stmt instanceof Result) calls
    - Fix Drupal 9.x coding standard whitespace
    - Untrack and gitignore composer.lock (library)

--- src/Storage/DbalNestedSet.php
+++ src/Storage/DbalNestedSet.php
@@ -2,7 +2,6 @@
 
 namespace PNX\NestedSet\Storage;
 
-use Doctrine\DBAL\Result;
 use PNX\NestedSet\NestedSetInterface;
 use PNX\NestedSet\Node;
 use PNX\NestedSet\NodeKey;
@@ -88,7 +87,8 @@ class DbalNestedSet extends BaseDbalStorage implements NestedSetInterface {
     catch (\Exception $e) {
       $this->connection->rollBack();
       throw $e;
-    } finally {
+    }
+    finally {
       $this->connection->setAutoCommit(TRUE);
     }
     return $newNode;
@@ -151,7 +151,6 @@ class DbalNestedSet extends BaseDbalStorage implements NestedSetInterface {
         ->setParameter('depth', $start + $depth - 1);
     }
     $stmt = $query->executeQuery();
-    assert($stmt instanceof Result);
     while ($row = $stmt->fetchAssociative()) {
       $descendants[] = new Node(new NodeKey($row['id'], $row['revision_id']), $row['left_pos'], $row['right_pos'], $row['depth']);
     }
@@ -187,7 +186,6 @@ class DbalNestedSet extends BaseDbalStorage implements NestedSetInterface {
     $stmt = $this->connection->executeQuery('SELECT parent.id, parent.revision_id, parent.left_pos, parent.right_pos, parent.depth FROM ' . $this->tableName . ' AS child, ' . $this->tableName . ' AS parent WHERE child.left_pos BETWEEN parent.left_pos AND parent.right_pos AND child.id = ? AND child.revision_id = ? ORDER BY parent.left_pos',
       [$nodeKey->getId(), $nodeKey->getRevisionId()]
     );
-    assert($stmt instanceof Result);
     while ($row = $stmt->fetchAssociative()) {
       $ancestors[] = new Node(new NodeKey($row['id'], $row['revision_id']), $row['left_pos'], $row['right_pos'], $row['depth']);
     }
@@ -223,7 +221,6 @@ class DbalNestedSet extends BaseDbalStorage implements NestedSetInterface {
   public function getTree(): array {
     $tree = [];
     $stmt = $this->connection->executeQuery('SELECT id, revision_id, left_pos, right_pos, depth FROM ' . $this->tableName . ' ORDER BY left_pos');
-    assert($stmt instanceof Result);
     while ($row = $stmt->fetchAssociative()) {
       $tree[] = new Node(new NodeKey($row['id'], $row['revision_id']), $row['left_pos'], $row['right_pos'], $row['depth']);
     }
@@ -269,7 +266,8 @@ class DbalNestedSet extends BaseDbalStorage implements NestedSetInterface {
     catch (\Exception $e) {
       $this->connection->rollBack();
       throw $e;
-    } finally {
+    }
+    finally {
       $this->connection->setAutoCommit(TRUE);
     }
 
@@ -306,7 +304,8 @@ class DbalNestedSet extends BaseDbalStorage implements NestedSetInterface {
     catch (\Exception $e) {
       $this->connection->rollBack();
       throw $e;
-    } finally {
+    }
+    finally {
       $this->connection->setAutoCommit(TRUE);
     }
 
@@ -436,7 +435,8 @@ class DbalNestedSet extends BaseDbalStorage implements NestedSetInterface {
     catch (\Exception $e) {
       $this->connection->rollBack();
       throw $e;
-    } finally {
+    }
+    finally {
       $this->connection->setAutoCommit(TRUE);
     }
 
